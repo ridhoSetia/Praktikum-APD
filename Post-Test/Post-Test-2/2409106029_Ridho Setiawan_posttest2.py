d79e906f9ac4e0d5f20629b6f0afe7824f787319
ridhosetiawan <ridhosetiawan24406@gmail.com>
2024-09-15 16:52:42 +0800

    revisi pdf, flowgorithm, dan codingan

--- Post-Test/Post-Test-2/2409106029_Ridho Setiawan_posttest2.py
+++ Post-Test/Post-Test-2/2409106029_Ridho Setiawan_posttest2.py
@@ -1,10 +1,10 @@
-hargaBeras = int(input("Masukkan harga beras : "))
 inputNama = input("Masukkan nama kamu : ")
 inputNIM = input("Masukkan NIM kamu : ")
+hargaBeras = int(input("Masukkan harga beras : "))
 
-diskonBerasMawar = int(hargaBeras - (0.11/100 * hargaBeras))
-diskonBerasSani = int(hargaBeras - (0.14/100 * hargaBeras))
-diskonBerasMaknyus = int(hargaBeras - (0.17/100 * hargaBeras))
+diskonBerasMawar = int(hargaBeras - (0.11 * hargaBeras))
+diskonBerasSani = int(hargaBeras - (0.14 * hargaBeras))
+diskonBerasMaknyus = int(hargaBeras - (0.17 * hargaBeras))
 
 print(f"{inputNama} dengan NIM {inputNIM} ingin membeli beras seharga Rp{hargaBeras}") 
 print(f"Jika dia membeli beras Mawar ia harus membayar Rp{diskonBerasMawar} Setelah mendapat diskon 11%.")
