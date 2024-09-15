eb5f42146c3ba1b57d0df1b0f1c02aecd6eb2619
ridhosetiawan <ridhosetiawan24406@gmail.com>
2024-09-16 07:13:20 +0800

    change variable name

--- Post-Test/Post-Test-2/2409106029_Ridho Setiawan_A2'24_posttest2.py
+++ Post-Test/Post-Test-2/2409106029_Ridho Setiawan_A2'24_posttest2.py
@@ -1,12 +1,21 @@
+# input nama lengkap
 inputNama = input("Masukkan nama kamu : ")
+# input NIM
 inputNIM = input("Masukkan NIM kamu : ")
+# input harga beras (400000) dan mengubah nilai harga beras menjadi integer
 hargaBeras = int(input("Masukkan harga beras : "))
 
-diskonBerasMawar = int(hargaBeras - (0.11 * hargaBeras))
-diskonBerasSani = int(hargaBeras - (0.14 * hargaBeras))
+# melakukan perhitungan pada masing-masing jenis beras, lalu mengubah nilainya menjadi integer
+
+# 0.11 = diskon beras mawar yaitu 11%
+hargaBerasMawar = int(hargaBeras - (0.11 * hargaBeras))
+# 0.14 = diskon beras mawar yaitu 14%
+hargaBerasSani = int(hargaBeras - (0.14 * hargaBeras))
+# 0.17 = diskon beras mawar yaitu 17%
 diskonBerasMaknyus = int(hargaBeras - (0.17 * hargaBeras))
 
+# menampilkan output
 print(f"{inputNama} dengan NIM {inputNIM} ingin membeli beras seharga Rp{hargaBeras}") 
-print(f"Jika dia membeli beras Mawar ia harus membayar Rp{diskonBerasMawar} Setelah mendapat diskon 11%.")
-print(f"Jika dia membeli beras Sani ia harus membayar Rp{diskonBerasSani} Setelah mendapat diskon 14%.")
+print(f"Jika dia membeli beras Mawar ia harus membayar Rp{hargaBerasMawar} Setelah mendapat diskon 11%.")
+print(f"Jika dia membeli beras Sani ia harus membayar Rp{hargaBerasSani} Setelah mendapat diskon 14%.")
 print(f"Jika dia membeli beras Maknyus ia harus membayar Rp{diskonBerasMaknyus} Setelah mendapat diskon 17%.")
