8ed11e98cd6189dc99cbeac2a669a1dfff8b10be
ridhosetiawan <ridhosetiawan24406@gmail.com>
2024-09-15 16:34:33 +0800

    perbaiki kode pada bagian print

--- Post-Test/Post-Test-2/2409106029_Ridho Setiawan_posttest2.py
+++ Post-Test/Post-Test-2/2409106029_Ridho Setiawan_posttest2.py
@@ -6,9 +6,7 @@ diskonBerasMawar = int(hargaBeras - (0.11/100 * hargaBeras))
 diskonBerasSani = int(hargaBeras - (0.14/100 * hargaBeras))
 diskonBerasMaknyus = int(hargaBeras - (0.17/100 * hargaBeras))
 
-print(f"""
-{inputNama} dengan NIM {inputNIM} ingin membeli beras seharga Rp{hargaBeras}
-Jika dia membeli beras Mawar ia harus membayar Rp{diskonBerasMawar} Setelah mendapat diskon 11%.
-Jika dia membeli beras Sani ia harus membayar Rp{diskonBerasSani} Setelah mendapat diskon 14%.
-Jika dia membeli beras Maknyus ia harus membayar Rp{diskonBerasMaknyus} Setelah mendapat diskon 17%.
-""")
+print(f"{inputNama} dengan NIM {inputNIM} ingin membeli beras seharga Rp{hargaBeras}") 
+print(f"Jika dia membeli beras Mawar ia harus membayar Rp{diskonBerasMawar} Setelah mendapat diskon 11%.")
+print(f"Jika dia membeli beras Sani ia harus membayar Rp{diskonBerasSani} Setelah mendapat diskon 14%.")
+print(f"Jika dia membeli beras Maknyus ia harus membayar Rp{diskonBerasMaknyus} Setelah mendapat diskon 17%.")
